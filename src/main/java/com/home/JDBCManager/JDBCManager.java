bfd1ab4ef61e6f186e995396f80f41b22611040b
Mikhail <anohin04@mail.ru>
2022-06-22 21:17:04 +0500

    Added editing and deleting rows from MySql tables

--- src/main/java/com/home/JDBCManager/JDBCManager.java
+++ src/main/java/com/home/JDBCManager/JDBCManager.java
@@ -2,7 +2,10 @@ package com.home.JDBCManager;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class JDBCManager {
@@ -24,8 +27,13 @@ public class JDBCManager {
 			
 			String request = "insert into " + tableName + " values(";
 			
-			for(String field : row) {
-				request += " \'" + field + "\'";
+			int size = row.size();
+			
+			for(int i = 0; i < size; i++) {
+				request += "\'" + row.get(i) + "\'";
+				if(i != size - 1) {
+					request += ", ";
+				}
 			}
 			
 			request += ");";
@@ -42,5 +50,127 @@ public class JDBCManager {
 			ex.printStackTrace();
 			return false;
 		}
+	}
+	
+	public boolean deleteRowFromTable(List<String> row, String tableName) {
+		try(Connection connection = DriverManager.getConnection(url, login, password)) {
+			Statement stat = connection.createStatement();
+			
+			List<String> columnNames = getColumnsNamesFromTable(tableName);
+			
+			String request = "DELETE FROM " + tableName + " WHERE ";
+			
+			int size = columnNames.size();
+			
+			for(int i = 0; i < size; i++) {
+				request += columnNames.get(i) + " = \'" + row.get(i) + "\' ";
+				if(i != size - 1) {
+					request += "AND ";
+				}
+			}
+			
+			request += ";";
+			
+			int result = stat.executeUpdate(request);
+			
+			if(result > 0) {
+				return true;
+			} else {
+				return false;
+			}
+			
+		} catch(Exception ex) {
+			ex.printStackTrace();
+			return false;
+		}
+	}
+	
+	public boolean editRowFromTable(List<String> oldRow, List<String> newRow, String tableName) {
+		try(Connection connection = DriverManager.getConnection(url, login, password)) {
+			
+			Statement stat = connection.createStatement();
+			
+			List<String> columnNames = getColumnsNamesFromTable(tableName);
+			
+			String request = "UPDATE " + tableName + " SET ";
+			
+			int size = columnNames.size();
+			
+			for(int i = 0; i < size; i++) {
+				request += columnNames.get(i) + " = \'" + newRow.get(i) + "\'";
+				
+				if(i != size - 1) {
+					request += ", ";
+				}
+			}
+			
+			request += " WHERE ";
+			
+			for(int i = 0; i < size; i++) {
+				request += columnNames.get(i) + " = \'" + oldRow.get(i) + "\'";
+				
+				if(i != size - 1) {
+					request += "AND ";
+				}
+			}
+			
+			request += ";";
+			
+			int result = stat.executeUpdate(request);
+			
+			if(result > 0) {
+				return true;
+			} else {
+				return false;
+			}
+			
+		} catch(Exception ex) {
+			ex.printStackTrace();
+			return false;
+		}
+	}
+	
+	private boolean clearTable(String tableName) {
+		try(Connection connection = DriverManager.getConnection(url, login, password)) {
+			
+			Statement stat = connection.createStatement();
+			
+			String request = "DELETE FROM " + tableName + ";";
+			
+			int result = stat.executeUpdate(request);
+			
+			if(result > 0) {
+				return true;
+			} else {
+				return false;
+			}
+			
+			
+		} catch(Exception ex) {
+			ex.printStackTrace();
+			return false;
+		}
+	}
+	
+	private List<String> getColumnsNamesFromTable(String tableName) {
+		List<String> columnNames = new ArrayList<>();
+		
+		try(Connection connection = DriverManager.getConnection(url, login, password)) {
+			Statement stat = connection.createStatement();
+						
+			String requestForNamesColumns = "SELECT * FROM " + tableName + ";"; //getting columns' names
+			ResultSet rs = stat.executeQuery(requestForNamesColumns);
+			ResultSetMetaData rsMetaData = rs.getMetaData();
+			int countColumns = rsMetaData.getColumnCount();
+			
+			for(int i = 1; i <= countColumns; i++) { //columns' names to List
+				columnNames.add(rsMetaData.getColumnName(i));
+			}
+			
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		
+		return columnNames;
 	}
 }
